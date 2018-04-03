8c81ac02311b649a608d1d4ca25e3ebd90961d90
ReubenSiama <reubenlalhmunsiama@gmail.com>
2018-04-03 15:56:36 +0530

    Delete prompt

--- resources/views/viewallprojects.blade.php
+++ resources/views/viewallprojects.blade.php
@@ -92,7 +92,25 @@
 							</td>
 							@if(Auth::user()->group_id == 1)
 							<td>
-								<a class="btn btn-xs btn-danger" href="{{ URL::to('/') }}/deleteProject?projectId={{ $project->project_id }}">Delete</a>
+								<button type="button" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#delete{{ $project->project_id }}">Delete</button>
+								<!-- Modal -->
+							  <div class="modal fade" id="delete{{ $project->project_id }}" role="dialog">
+							    <div class="modal-dialog modal-sm">
+							      <div class="modal-content">
+							        <div class="modal-header">
+							          <button type="button" class="close" data-dismiss="modal">&times;</button>
+							          <h4 class="modal-title">Delete</h4>
+							        </div>
+							        <div class="modal-body">
+							          <p>Are you sure you want to delete this project?</p>
+							        </div>
+							        <div class="modal-footer">
+							        	<a class="btn btn-danger pull-left" href="{{ URL::to('/') }}/deleteProject?projectId={{ $project->project_id }}">Yes</a>
+							          <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+							        </div>
+							      </div>
+							    </div>
+							  </div>
 							</td>
 							@endif
 						</tr>
