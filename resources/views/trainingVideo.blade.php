136f8913771f5fa92f7e0bf384b5f9db9792129f
Mamahome developement <34155363+Mamahome@users.noreply.github.com>
2018-04-13 12:52:10 +0530

    Update trainingVideo.blade.php

--- resources/views/trainingVideo.blade.php
+++ resources/views/trainingVideo.blade.php
@@ -11,7 +11,7 @@
 <div class="container">
 <div class="col-md-6">
     <div class="panel panel-default" style="border-color:green;">
-                <div class="panel-heading" style="background-color: green;color:white;"><b> View Training Videos</b> <br> Please select the Department and desgination in order to get the training videos
+                <div class="panel-heading" style="background-color: green;color:white;"><b> View Training Videos</b> <br> Please select the Department and designation in order to get the training videos
                     @if(session('ErrorFile'))
                         <div class="alert-danger pull-right">{{ session('ErrorFile' )}}</div>
                     @endif 
