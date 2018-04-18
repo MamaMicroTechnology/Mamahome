ffded39f858138c17a1b7c67fa2d06010b8c6ae0
ReubenSiama <reubenlalhmunsiama@gmail.com>
2018-04-18 09:43:18 +0530

    Correction

--- resources/views/scdashboard.blade.php
+++ resources/views/scdashboard.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <br><br>
 <div style="background-color:white" class="container" >
-<h2 ><center>WELCOME TO {{ Auth::user()->group_id == 16 ? ' SALES CONVERTER' : 'ASSISTANT MANAGER OF SALES AND MARKETING' }}
+<h2 ><center>WELCOME TO {{ Auth::user()->group_id == 17 ? ' SALES CONVERTER' : 'ASSISTANT MANAGER OF SALES AND MARKETING' }}
 <br>ZONE 1, BANGALORE'S DASHBOARD
 <BR><BR>
     <SMALL>You must know your responsibilities and carry out your tasks responsibly.<br>
