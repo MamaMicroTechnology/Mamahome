f3b0d5a5dd8d2439b1f7cfe27f524a2ed08f0eba
ReubenSiama <reubenlalhmunsiama@gmail.com>
2018-04-18 09:38:25 +0530

    Daily slots link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -391,6 +391,7 @@ div#calendar{
     <a href="{{ URL::to('/') }}/kra">KRA</a>
     <a href="{{ URL::to('/projectDetailsForTL') }}">Project Search</a>
     <a href="{{ URL::to('/') }}/enquirysheet">Enquiry Sheet</a>
+    <a href="{{ URL::to('/dailyslots') }}">Daily Slots</a>
   </div>
 @endif
 @endif
