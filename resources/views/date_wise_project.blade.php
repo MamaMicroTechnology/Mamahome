fd814a828a26bdb44f6d43a679ea358d2d6ef40f
MuraliHamsa <muralikncse@gmail.com>
2018-04-30 10:28:42 +0530

    changes

--- resources/views/date_wise_project.blade.php
+++ resources/views/date_wise_project.blade.php
@@ -48,7 +48,7 @@
                                     {{ csrf_field() }}
                                     <input type="hidden" value="{{ $project->project_id }}" name="id">
                                     <div class="checkbox">
-                                        <label><input type="checkbox" {{ $project->confirmed !== "0" ? 'checked': ''}} name="confirmed" onchange="this.form.submit()">Called</label>
+                                        <label><input type="checkbox" {{ $project->confirmed !== "0" ||  $project->confirmed == "true" ? 'checked': ''}}  name="confirmed" onchange="this.form.submit()">Called</label>
                                     </div>
                                     </div>       
                                 </form>
