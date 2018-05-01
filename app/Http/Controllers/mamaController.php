53f0c66c5db6389bd310895b3d141de1d5975e6f
Mamahome developement <34155363+Mamahome@users.noreply.github.com>
2018-05-01 15:42:37 +0530

    Update mamaController.php

--- app/Http/Controllers/mamaController.php
+++ app/Http/Controllers/mamaController.php
@@ -1482,7 +1482,7 @@ class mamaController extends Controller
             'main_category' => $categoryNames,
             'brand' => $brandnames,
             'sub_category'  =>$subcategories,
-            'generated_by' =>$request->initiator->id,
+            'generated_by' =>$request->id,
             'notes' => $request->eremarks,
             'quantity' => $request->equantity,
             'requirement_date' => $request->edate
