dc03df2d4319a18b28a9885ebef377917f9d7dae
ReubenSiama <reubenlalhmunsiama@gmail.com>
2018-05-30 12:23:01 +0530

    Correction on sales

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2246,7 +2246,7 @@ class HomeController extends Controller
         }
         $projects = ProjectDetails::whereIn('project_id',$projectids)
                     ->where('quality',"Unverified")
-                    ->orWhere('updated_at','LIKE',date('Y-m-d')."%")
+                    // ->Where('updated_at','LIKE',date('Y-m-d')."%")
                     ->select('project_details.*','project_id')
                     ->orderBy('project_id','ASC')
                     ->paginate(15);
