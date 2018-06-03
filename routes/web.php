70fd838f3f5206941e152d2a104f65de160424d1
Mamahome developement <34155363+Mamahome@users.noreply.github.com>
2018-06-03 23:33:37 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -450,12 +450,20 @@ Route::group(['middleware'=>['asst']],function(){
     // not working
 });
 Route::group(['middleware'=>['AccountExecutive']],function(){
-    Route::get('/accountExecutive','aeController@getAccountExecutive');
+    //Route::get('/accountExecutive','aeController@getAccountExecutive');
+    Route::get('/accountExecutive','HomeController@leDashboard');
     Route::get('/builderprojects','aeController@viewBuilderProjects');
     Route::get('/addBuilderProjects','aeController@addBuilderProjects');
     Route::post('/addBuilderDetails','aeController@postBuilderDetails');
     Route::post('/addBuilderProject','aeController@addBuilderProject');
     Route::get('/deliveredOrders','aeController@getDeliveredOrders');
+    
+    Route::get('/accountlistingEngineer','HomeController@listingEngineer');
+    Route::get('/accountroads','HomeController@getRoads');
+    Route::get('/accountrequirementsroads','HomeController@getRequirementRoads');
+    Route::get('/accountreports','HomeController@getMyReports');
+    
+    
 });
 //Logistics
 Route::group(['middleware'=>['Logistics']],function(){
