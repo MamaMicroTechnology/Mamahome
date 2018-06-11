9b29054c71e4b61bd825a021342453fb3369f7bb
MuraliHamsa <muralikncse@gmail.com>
2018-06-11 16:36:19 +0530

    Murali's codes

--- routes/web.php
+++ routes/web.php
@@ -487,6 +487,7 @@ Route::group(['middleware'=>['Logistics']],function(){
     Route::get('/deliveredorders','logisticsController@deliveredorders');
     Route::get('/takesignature','logisticsController@takesignature');
     Route::post('/saveSignature','logisticsController@saveSignature');
+    Route::post('/payment','logisticsController@payment');
     Route::post('/saveDeliveryDetails','logisticsController@saveDeliveryDetails');
 });
 
