e4787fbe34631c16a2ed8e521dea62ea91eb2f57
chaithranagaraj <37991787+chaithranagaraj@users.noreply.github.com>
2018-06-19 17:01:01 +0530

    Update teamLeader.blade.php

--- resources/views/teamLeader.blade.php
+++ resources/views/teamLeader.blade.php
@@ -42,7 +42,7 @@
         
         
             @foreach($loggedInUsers as $loggedInUser)
-            @if($loggedInUser->id = 1 || $loggedInUser->id = 2)
+            @if( $loggedInUser->id = 2)
                 <tr>
                     <td>{{ $loggedInUser->empId }}</td>
                     <td>{{ $loggedInUser->name }}</td>
@@ -53,7 +53,7 @@
                 
             @endforeach
             @foreach($leLogins as $leLogin)
-            @if($leLogin->id ==1 || $leLogin->id == 2)
+            @if( $leLogin->id == 2)
                 <tr>
                     <td>{{ $leLogin->employeeId }}</td>
                     <td>{{ $leLogin->name }}</td>
