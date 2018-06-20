c9c441c9dc36d324e52bf6844a63906a3c086f34
muraliyadav <34570686+MuraliHamsa@users.noreply.github.com>
2018-06-20 15:17:19 +0530

    Update listingEngineerDashboard.blade.php

--- resources/views/listingEngineerDashboard.blade.php
+++ resources/views/listingEngineerDashboard.blade.php
@@ -152,15 +152,10 @@
     });
 
     var infowindow = new google.maps.InfoWindow();
-    var everythingElse = [
-      new google.maps.LatLng(0, -90),
-      new google.maps.LatLng(0, 90),
-      new google.maps.LatLng(90, -90),
-      new google.maps.LatLng(90, 90),
-    ];
+   
     var marker, i;
     var subward = new google.maps.Polygon({
-        paths: [everythingElse, newpath],
+        paths:  newpat,
         strokeColor: '#'+col,
         strokeOpacity: 1,
         strokeWeight: 2,
