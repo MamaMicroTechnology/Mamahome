c8b7bd92a9e17184788c9ebe7656bcd505944fa0
muraliyadav <34570686+MuraliHamsa@users.noreply.github.com>
2018-06-27 17:01:27 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -780,7 +780,7 @@ class HomeController extends Controller
                     ->leftjoin('site_engineer_details','requirements.project_id','site_engineer_details.project_id')
                     ->leftjoin('consultant_details','requirements.project_id','consultant_details.project_id')
                     ->leftjoin('site_addresses','requirements.project_id','=','site_addresses.project_id')
-                    ->select('requirements.*','users.name','project_details.project_name','procurement_details.procurement_contact_no','site_addresses.address','contractor_details.contractor_contact_no','owner_details.owner_contact_no','site_engineer_details.site_engineer_contact_no','consultant_details.consultant_contact_no')
+                    ->select('requirements.*','users.name','project_details.project_name','procurement_details.procurement_contact_no','site_addresses.address','contractor_details.contractor_contact_no','owner_details.owner_contact_no','site_engineer_details.site_engineer_contact_no','consultant_details.consultant_contact_no','requirements.id')
                     ->first();
                    
         return view('editEnq',['enq'=>$enq,'category'=>$category,'users'=>$users]);
