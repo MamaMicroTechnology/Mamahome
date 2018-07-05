c927ce4756f1ab491b7c27696103969cfac153d2
chaithranagaraj <37991787+chaithranagaraj@users.noreply.github.com>
2018-07-05 09:38:49 +0530

    Update formeremp.blade.php

--- resources/views/formeremp.blade.php
+++ resources/views/formeremp.blade.php
@@ -81,8 +81,8 @@ img {
 <div id="name">
 @foreach($users as $user)
   <a href="#">
-    <div style="overflow: hidden;" class="col-md-3 col-md-offset-1 img-thumbnail">
-    <center><img class="img1" src="http://localhost/mamahome/public/assetbill/1530183607.jpg" width="100" height="100">
+    <div style="overflow: hidden;" class="col-md-3 col-md-offset-1 ">
+    <center><img class="img1" src="{{ URL::to('/') }}/public/profilePic/{{ $user->profilepic }}" width="100" height="100">
       <p style="text-align: center;">{{ $user->name }}</p>
       <small>{{ $user->email }}</small>
     </center>
