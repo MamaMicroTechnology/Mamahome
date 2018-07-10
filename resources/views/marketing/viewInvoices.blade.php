91e1e675fb055372062c232f10565f22891855c4
muraliyadav <34570686+MuraliHamsa@users.noreply.github.com>
2018-07-10 17:08:07 +0530

    Update viewInvoices.blade.php

--- resources/views/marketing/viewInvoices.blade.php
+++ resources/views/marketing/viewInvoices.blade.php
@@ -3,21 +3,45 @@
 
 <div class="col-md-10 col-md-offset-1">
     <div class="panel panel-primary">
-        <div class="panel-heading">Invoices</div>
+        <div class="panel-heading"><center>Total Invoices : {{  $invoice }}</center></div>
         <div class="panel-body">
+          <b>Category Wise Invoices : {{$total}}</b>
+ <center>  <form action="{{ URL::to('/') }}/viewInvoices" method="get" >
+        <b> Select Category:</b>   <select class="form-control" name="cat" onchange="form.submit()" style="width:30%;">
+              <option value="select">----Select category----</option>
+              @foreach($cat as $cate)
+              <option value="{{ $cate->category_name }}">{{ $cate->category_name }}</option>
+              @endforeach
+
+          </select>
+</form></center>
         <table class="table table-hover">
             <thead>
                 <th>Invoice No</th>
+                <th>OrderId</th>
+                <th>ProjectId</th>
                 <th>Location</th>
                 <th>Name</th>
-                <th>Date</th>
+                <th>Delivery Date</th>
+               
             </thead>
-            @foreach($invoices as $invoice)
+            @foreach($inc as $invoice)
                 <tr>
                     <td><a href="{{ URL::to('/') }}/invoice?id={{ $invoice->invoice_id }}">{{ $invoice->invoice_id }}</td>
+                    <td>{{ $invoice->requirement_id }}</td>
+                    <td>
+                        
+                    <a href="{{ URL::to('/') }}/admindailyslots?projectId={{ $invoice->project_id }}" target="_blank">{{ $invoice->project_id }} </a>
+                    </td>
+                    
                     <td>{{ $invoice->deliver_location }}</td>
                     <td>{{ $invoice->customer_name }}</td>
-                    <td>{{ $invoice->delivery_date }}</td>
+                    <td>
+                                              {{ date('d-m-Y', strtotime($invoice->delivery_date)) }}
+                                            </td>
+                                          
+                    
+                  
                 </tr>
             @endforeach
         </table>
