c94003e88a5abf2e454a422d954bc5613f1c957f
ReubenSiama <reubenlalhmunsiama@gmail.com>
2018-07-13 22:50:32 +0530

    s

--- resources/views/projection/country.blade.php
+++ resources/views/projection/country.blade.php
@@ -39,8 +39,8 @@
             <tr>
                 <td style="text-align:center">'B' Grade Zones</td>
                 <td style="text-align:center">46</td>
-                <td style="text-align:center">{{ number_format(($planning->totalTarget * 46)/100*30) }}</td>
-                <td style="text-align:center">{{ number_format(($planning->totalTP * 46)/100*30) }}</td>
+                <td style="text-align:center">{{ number_format(($planning->totalTarget * 46)/100*70) }}</td>
+                <td style="text-align:center">{{ number_format(($planning->totalTP * 46)/100*70) }}</td>
             </tr>
             <tr>
                 <td style="text-align:center">'C' Grade Zones</td>
@@ -51,14 +51,14 @@
             <tr>
                 <td style="text-align:center">'D' Grade Zones</td>
                 <td style="text-align:center">58</td>
-                <td style="text-align:center">{{ number_format(($planning->totalTarget * 58)/100*90) }}</td>
-                <td style="text-align:center">{{ number_format(($planning->totalTP * 58)/100*90) }}</td>
+                <td style="text-align:center">{{ number_format(($planning->totalTarget * 58)/100*10) }}</td>
+                <td style="text-align:center">{{ number_format(($planning->totalTP * 58)/100*10) }}</td>
             </tr>
             <tr>
                 <th style="text-align:center"></th>
                 <th style="text-align:center">Total</th>
-                <th style="text-align:center">{{ number_format(($planning->totalTarget * 30) + (($planning->totalTarget * 46)/100*30) + (($planning->totalTarget * 58)/100*50) + (($planning->totalTarget * 58)/100*90)) }}</th>
-                <th style="text-align:center">{{ number_format(($planning->totalTP * 30) + (($planning->totalTP * 46)/100*30) + (($planning->totalTP * 58)/100*50) + (($planning->totalTP * 58)/100*90)) }}</th>
+                <th style="text-align:center">{{ number_format(($planning->totalTarget * 30) + (($planning->totalTarget * 46)/100*70) + (($planning->totalTarget * 58)/100*50) + (($planning->totalTarget * 58)/100*10)) }}</th>
+                <th style="text-align:center">{{ number_format(($planning->totalTP * 30) + (($planning->totalTP * 46)/100*70) + (($planning->totalTP * 58)/100*50) + (($planning->totalTP * 58)/100*10)) }}</th>
             </tr>
         </table>
     </div>
