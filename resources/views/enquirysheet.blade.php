41e3062fa6571ce7042f5ebf09224712f1f33cea
chaithranagaraj <37991787+chaithranagaraj@users.noreply.github.com>
2018-07-20 20:02:50 +0530

    Update enquirysheet.blade.php

--- resources/views/enquirysheet.blade.php
+++ resources/views/enquirysheet.blade.php
@@ -284,7 +284,7 @@ function myFunction() {
 	  }
 	}else{
 		for (i = 0; i < tr.length; i++) {
-	    td = tr[i].getElementsByTagName("td")[11];
+	    td = tr[i].getElementsByTagName("td")[12];
 	    if (td) {
 	      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
 	        tr[i].style.display = "";
