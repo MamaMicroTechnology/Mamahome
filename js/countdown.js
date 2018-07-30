6c5e186dc7921866e9a9dff9384b4bb153e63cc2
chaithranagaraj <37991787+chaithranagaraj@users.noreply.github.com>
2018-07-30 16:35:13 +0530

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
 	var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = '2018-08-31';
+var deadline = '2018-09-31';
 initializeClock('clockdiv', deadline);
