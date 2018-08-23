e2fb4d8300344bad21a38622249d2f09c36adb30
ReubenSiama <reubenlalhmunsiama@gmail.com>
2018-08-23 13:40:51 +0530

    Corection

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -658,7 +658,7 @@ public function getproject(request $request){
                         $field->logindate = date('Y-m-d');
                         $field->logintime = date(' H:i A');
                         $field->remark = $request->remark;
-                        $field->latitude = $request->;
+                        $field->latitude = $request->latitude;
                         $field->longitude = $request->longitude;
                         $field->address = $request->address;
                         $field->save();
