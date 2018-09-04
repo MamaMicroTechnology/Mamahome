4e5aed2adade35b3a6fdbec690cd30b889bf47f5
muraliyadav <34570686+MuraliHamsa@users.noreply.github.com>
2018-09-04 12:03:32 +0530

    Update TokenController.php

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -408,7 +408,7 @@ public function enquiry(request $request){
     public function updateEnquiry(request $request){
         
        
-        $enquiry = Requirement::where('id',$request->id)->first();
+            $enquiry = Requirement::where('id',$request->id)->first();
                 $enquiry->project_id = $request->project_id;
                 $enquiry->main_category = $request->main_category;
                 $enquiry->brand = $request->brand;
@@ -418,7 +418,7 @@ public function enquiry(request $request){
                 $enquiry->A_contact = $request->A_contact;
                 $enquiry->quantity = $request->quantity;
                 $enquiry->user_id = $request->userid;
-        $enquiry->save();
+              $enquiry->save();
                        
          
           if($enquiry->save()){
@@ -626,14 +626,14 @@ public function getproject(request $request){
       public function updateLocation(Request $request){
             $data = TrackLocation::where('user_id',$request->user_id)
                         ->where('date',$request->date)
-                        ->first();
+                        ->update([
             
-            $data->user_id = $request->user_id;
-            $data->lat_long = $request->lat_long;
-            $datea->time = $request->time;
-            $data->date = $request->date;
-            $data->kms = $request->kms;
-            $data->save();
+            $data->user_id => $request->user_id;
+            $data->lat_long => $request->lat_long;
+            $datea->time => $request->time;
+            $data->date => $request->date;
+            $data->kms => $request->kms;
+           )];                 
             if($data->save()){
                $responseData = array('success'=>'1', 'data'=>$data, 'message'=>"Location has been Updated successfully");
                $userResponse = json_encode($responseData);
