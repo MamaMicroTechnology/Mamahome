c83a5ee0a44596bdc046fda9c226baeacaaab9ae
MuraliHamsa <muralikncse@gmail.com>
2018-09-07 17:05:21 +0530

    add

--- resources/views/requirementsroad.blade.php
+++ resources/views/requirementsroad.blade.php
@@ -45,7 +45,7 @@
 </div>
 <div class="col-md-4" style="margin-left: 20%;">
 	<div class="panel panel-default">
-		<div class="panel-heading">Select<span class="pull-right">{{$gc}}</span></div>
+		<div class="panel-heading">Select</div>
 		<div class="panel-body">
 			<ul class="list-group">
 				<li class="list-group-item">
@@ -58,7 +58,7 @@
 			    <option value="">-----Select----</option>
 				<option value="Genuine">Genuine</option>
 					<option value="Unverified">Unverified</option>
-						<option value="Fake">Fake</option>
+						<!-- <option value="Fake">Fake</option> -->
 			</select> 
 			</form>
 		</div>
