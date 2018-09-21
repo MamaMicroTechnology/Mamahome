ec1b9f0d51cb22f706a0f63f8d121b08917168e7
Chaithra <chaithrashreen95@gmail.com>
2018-09-21 10:27:33 +0530

    activitylog

--- app/Vendor.php
+++ app/Vendor.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
 
 class Vendor extends Model
 {
     protected $table = 'vendor';
+    use LogsActivity;
+    protected  $fillable =[
+        'vendor_type',
+      ];
+   
+      protected static $logOnlyDirty = true; 
+      protected static $causerId = 3;
+      protected static $logName = "";
+      protected static $logFillable = true;
 }
--- app/Zone.php
+++ app/Zone.php
@@ -3,10 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
 
 class Zone extends Model
 {
     protected $table = 'zones';
+    use LogsActivity;
+    protected  $fillable =[
+       'country_id',
+'zone_name',
+'zone_number',
+'zone_image',
+      ];
+   
+      protected static $logOnlyDirty = true; 
+      protected static $causerId = 3;
+      protected static $logName = "";
+      protected static $logFillable = true;
     public function country()
     {
     	return $this->belongsTo('App\Country');
