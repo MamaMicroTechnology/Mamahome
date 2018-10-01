5946d33c3d5b6c6ae488ef3245d22cdc8aaa5b71
muraliyadav <34570686+MuraliHamsa@users.noreply.github.com>
2018-10-01 08:19:08 +0530

    Update manuenquiry.blade.php

--- resources/views/manuenquiry.blade.php
+++ resources/views/manuenquiry.blade.php
@@ -45,14 +45,15 @@ id="error"></div></td>
 NULL?$projects->proc->contact:$projects->contact_no }}</td>
 @endif
 </tr>
-<!-- <tr>
-<td><label>Name* : </label></td>
-<td><input required type="text" name="ename" id="ename"
-class="form-control"/></td>
-</tr> -->
 <tr>
 @if(!isset($_GET['projectId']))
-
+<td><label>Project* : </label></td>
+<td>
+<select required class="form-control" id='selectprojects'
+name="selectprojects" onchange="getAddress()">
+</select>
+</td>
+@else
 <td><label>Manufacturer ID : </label></td>
 <td >
 <input type="hidden" value="{{ $projects->id }}" name="manu_id">
