d2a9f0d5a35d6d781fc4bbb9fc2908575d745853
chaithra <chaithrashreen95@gmail.com>
2018-10-12 15:52:17 -0700

    c

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8445,8 +8445,8 @@ public function viewManufacturer1(Request $request)
   }
   public function sbreaktime(Request $request)
   {
-    $x = BreakTime::where('user_id',Auth::user()->id)->orderBy('user_id','ASC')->pluck('user_id')->first();
-    BreakTime::where('user_id',$x)->update([
+    $x = BreakTime::where('user_id',Auth::user()->id)->orderBy('user_id','ASC')->pluck('id')->first();
+    BreakTime::where('id',$x)->update([
             'stop_time' => date('h:i A')
         ]);
         return back()->with('Success','Your Break Time has been Ended');
