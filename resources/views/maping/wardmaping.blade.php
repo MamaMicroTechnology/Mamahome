e18967727246e9f9013e25c10f19d668930a9cef
chaithra <chaithrashreen95@gmail.com>
2018-10-15 08:36:28 -0700

    c

--- resources/views/maping/wardmaping.blade.php
+++ resources/views/maping/wardmaping.blade.php
@@ -1,116 +1,214 @@
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-<script type="text/javascript" src="{{asset('js/gmaps.js')}}"></script>
- <div class="col-md-12">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-primary">
-                <div class="panel-heading" style="color:white;font-size: 15px;">Map Of &nbsp;&nbsp;
-                    @if(session('Error'))
-                        <div class="alert-danger pull-right">{{ session('Error')}}</div>
-                    @endif
-                    <a  href="{{URL::to('/')}}/viewallProjects" class="btn btn-sm btn-danger pull-right">Back</a>    
-
-                </div>
-                <div class="panel-body">
-                    <div id="map" style="width:100%;height:400px;"></div>
-                </div>
-               </div>
-             </div>
-          </div>
-       </div>
-<script type="text/javascript" scr="https://maps.google.com/maps/api/js?sensor=false"></script>
-<script>
-function myMap() {
-      var mapProp= {
-          center:new google.maps.LatLng(12.9755324,77.610114),
-          zoom:5,
-      };
-var path = [];
-var newpath = [];
-var map=new google.maps.Map(document.getElementById("map"),mapProp);
-          $(document).ready(function(){
-
-      map = new GMaps({
-        el: '#map',
-        zoom: 11,
-        lat: 12.9716,
-        lng: 77.5946,
-        click: function(e){
-          latitude = e.latLng.lat();
-          lognitude = e.latLng.lng();
-          // console.log(latitude,lognitude);
-          path.push([latitude, lognitude]);
-          document.getElementById('path').value = path;
-          // console.log(path);
-          // polygon = map.drawPolyline({
-          //   path: path,
-          //   strokeColor: '#131540',
-          //   strokeOpacity: 0.6,
-          //   strokeWeight: 6
-          // });
+@extends('layouts.app')
+@section('content')
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+  <script type="text/javascript" src="{{asset('js/gmaps.js')}}"></script>
+  <script src="{{ URL::to('/') }}/js/jscolor.js"></script>
+  <!-- <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.3.0/bootstrap.min.css" /> -->
+  <link rel="stylesheet" href="{{ URL::to('/') }}/css/some.css" />
+  <link rel="stylesheet" href="{{ URL::to('/') }}/css/app.css" />
+  @if($zones->lat != null)
+  <script type="text/javascript">
+  var count = 0;
+    var map, path = [], newpath = [];
+    var marker = [];
+    var latlng = "{{ $zones-> lat }}";
+    $(document).ready(function(){
+      var places = latlng.split(",");
+        var mymarker = [];
+        var latt = 0,lngg = 0;
+        for(var i=0;i<places.length;i+=2){
+          newpath.push([parseFloat(places[i]), parseFloat(places[i+1])]);
+          latt += parseFloat(places[i]);
+          lngg += parseFloat(places[i+1]);
         }
+        latt = latt/newpath.length;
+        lngg = lngg/newpath.length;
+        var val = parseInt(document.getElementById('color').value);
+        var line = "#"+String(val - 10);
+        // for marking maps
+        map = new GMaps({
+          el: '#map',
+          lat: latt,
+          lng: lngg,
+          click: function(e){
+            latitude = e.latLng.lat();
+            lognitude = e.latLng.lng();
+            path.push([latitude, lognitude]);
+            marker[0] = path;
+            document.getElementById('path').value = marker;
+            polygon = map.removePolylines();
+            polygon = map.drawPolyline({
+              path: path,
+              strokeColor: '#131540',
+              strokeOpacity: 0.6,
+              strokeWeight: 1
+            });
+          }
+        });
+
+         $("#undo").click(function(){
+          path.pop();
+            marker[0] = path;
+            polygon = map.removePolylines();
+            document.getElementById('path').value = marker;
+            polygon = map.drawPolyline({
+              path: path,
+              strokeColor: '#131540',
+              strokeOpacity: 0.6,
+              strokeWeight: 1
+            });
+         });
+
+      map.setZoom(11);
+      var line = parseInt('{{ $zones->color }}') + 12345;
+      map.drawPolygon({
+        paths: newpath,
+        strokeColor: '#'+line,
+        strokeOpacity: 0.6,
+        fillColor: '#{{ $zones->color }}',
+        strokeWeight: 2
+      });
+      map.setOptions({draggableCursor:'crosshair'});
+      $("#draw").click(function(){
+        var val = parseInt(document.getElementById('color').value);
+        var color = "#"+document.getElementById('color').value;
+        document.getElementById('path').value = marker;
+        var line = "#"+String(val - 10);
+        count = 1;
+        polygon = map.removePolylines();
+        polygon = map.removePolygons();
+        polygon = map.drawPolygon({
+          paths: marker,
+          strokeColor: line,
+          strokeOpacity: 1,
+          strokeWeight: 1,
+          fillColor: color,
+          fillOpacity: 0.2
+        });
       });
     });
-    for(var i=0;i<path.length;i+=2){
-          newpath.push({lat: parseFloat(path[i]), lng: parseFloat(path[i+1])});
-    }
-          // console.log(newpath);
-        var directionsService = new google.maps.DirectionsService;
-
-        document.getElementById('submit').addEventListener('click', function() {
-          calculateAndDisplayRoute(directionsService);
+  </script>
+@else
+<script type="text/javascript">
+    var map, path = [], newpath = [];
+    var marker = [];
+    $(document).ready(function(){
+        var mymarker = [];
+        var val = parseInt(document.getElementById('color').value);
+        var line = "#"+String(val - 10);
+        // for marking maps
+        map = new GMaps({
+          el: '#map',
+          lat: 12.9716,
+          lng: 77.5946,
+          click: function(e){
+            latitude = e.latLng.lat();
+            lognitude = e.latLng.lng();
+            path.push([latitude, lognitude]);
+            marker[0] = path;
+            document.getElementById('path').value = marker;
+            polygon = map.removePolylines();
+            polygon = map.drawPolyline({
+              path: path,
+              strokeColor: '#131540',
+              strokeOpacity: 0.6,
+              strokeWeight: 1
+            });
+          }
         });
-      
+        $("#undo").click(function(){
+          path.pop();
+            marker[0] = path;
+            document.getElementById('path').value = marker;
+            polygon = map.removePolylines();
+            polygon = map.drawPolyline({
+              path: path,
+              strokeColor: '#131540',
+              strokeOpacity: 0.6,
+              strokeWeight: 1
+            });
+         });
+      map.setZoom(10);
+      map.setOptions({draggableCursor:'crosshair'});
+      $("#draw").click(function(){
+        var val = parseInt(document.getElementById('color').value);
+        var color = "#"+document.getElementById('color').value;
+        document.getElementById('path').value = marker;
+        var line = "#"+String(val - 10);
+        polygon = map.removePolylines();
+        polygon = map.removePolygons();
+        polygon = map.drawPolygon({
+          paths: marker,
+          strokeColor: line,
+          strokeOpacity: 1,
+          strokeWeight: 1,
+          fillColor: color,
+          fillOpacity: 0.2
+        });
+      });
+    });
+  </script>
+@endif
+</head>
+<body>
+<div class="container">
+  <div class="row">
+    <h1>{{ $page }} Mapping
+    </h1>
+    <div class="row">
+      <div class="span11">
+        <div id="map"></div>
+      </div>
+      <br>
+      <div class="col-md-12">
+        <form onsubmit="validate()" action="{{ URL::to('/') }}/saveMap" method="POST">
+          {{ csrf_field() }}
+            <div class="col-md-2">
+              {{ $page }}:<br>
+                <input type="text" name="name" id="name" value="{{ $zones->name }}" class="form-control">
+                <input type="hidden" name="page" value="{{ $page }}">
+                <input type="hidden" name="zone" value="{{ $zones->id }}">
+            </div>
+            <div class="col-md-2">
+              Color:
+              <input name="color" class="jscolor form-control" id="color" value="{{ $zones->color }}">
+              <div id="area"></div>
+            </div>
+            <div class="col-md-8">
+              <br>
+              <div class="col-md-8">
+                <input type="text" value="{{ $zones->lat }}" name="path" id="path" class="form-control">
+              </div>
+              <div class="col-md-4 btn-group">
+                <button type="button" class="btn btn-primary" id="undo">Undo</button>
+                <button type="button" id="draw" class="btn btn-primary">Preview</button>
+                <input type="submit" value="Save" class="btn btn-success" id="savebutton">
+              </div>
+            </div>
+        </form>
+      </div>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDGSf_6gjXK-5ipH2C2-XFI7eUxbHg1QTU"></script>
+  </div>
+</div>
 
-      function calculateAndDisplayRoute(directionsService) {
-        alert();
-        var waypts = [];
-        for(var i=1; i<path.length; i++){
-          waypts.push(path[i]);
+<script>
+  function getWards(){
+    var id = document.getElementById('Zones').value;
+    var wards = "";
+    $.ajax({
+        type: 'GET',
+        url: "{{URL::to('/')}}/getWards",
+        data: {id:id},
+        async: false,
+        success: function(response)
+        {
+          for(var i = 0; i < response.length; i++){
+            wards += "<option value="+response[i].id+" onclick=makelines('"+response[i].lat+"')>"+response[i].ward_name+"</option>"
+          }
+          document.getElementById('wards').innerHTML = wards;
+          console.log(response);
         }
-        console.log(waypts);
-        directionsService.route({
-          origin: path[0],
-          destination: path[0],
-          waypoints: waypts,
-          optimizeWaypoints: true,
-          travelMode: 'DRIVING'
-        }, function(response, status) {
-          console.log(resonse);
-          // if (status === 'OK') {
-          //   directionsDisplay.setDirections(response);
-          //   var route = response.routes[0];
-          //   var summaryPanel = document.getElementById('directions-panel');
-          //   summaryPanel.innerHTML = '';
-          //   // For each route, display summary information.
-          //   for (var i = 0; i < route.legs.length; i++) {
-          //     var routeSegment = i + 1;
-          //     summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
-          //         '</b><br>';
-          //     summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-          //     summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-          //     summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
-          //   }
-          // } else {
-          //   window.alert('Directions request failed due to ' + status);
-          // }
-        });
- 
-   }     
-}
+    });
+  }
 </script>
-  <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDGSf_6gjXK-5ipH2C2-XFI7eUxbHg1QTU&callback=myMap"></script></script> -->
-  <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjnJY74MIHYFBW8V2-c4mL3KCJXZ4_2mw&callback=myMap">
-    </script>
-  <div class="col-md-4">
-  <input type="submit" name="submit" id="submit" class="form-control">
-</div>
-  <div class="col-md-4">
-                <input type="text" value="" name="path" style="width:1000px;height: 100px;" id="path" class="form-control">
-                
-  </div>
-  <div class="col-md-4">
-                <input type="text" name="waypoints" id="waypoints" class="form-control">
-  </div>
-</div>
+@endsection
