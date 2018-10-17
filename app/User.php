32429fdb5881ed8a2eddc7d486d845daf370211f
chaithra <chaithrashreen95@gmail.com>
2018-10-17 13:47:59 +0530

    c

--- app/User.php
+++ app/User.php
@@ -39,6 +39,7 @@ class User extends Authenticatable
     {
         return $this->belongsTo('App\Group');
     }
+    
     public function requirement(){
         return $this->hasOne('App\Requirement','id','generated_by');
     }
