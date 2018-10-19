db80a124950f120deaac52e186327f3fe8311dfc
Reuben <reubenlalhmunsiama@gmail.com>
2018-10-19 11:52:43 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -73,3 +73,4 @@ Route::get('/fakegps','TokenController@fakegps');
 
 //listing engineers
 Route::post('/addleProject','TokenController@addleProject');
+Route::post('/saveManufacturer','mamaController@postSaveManufacturer');
