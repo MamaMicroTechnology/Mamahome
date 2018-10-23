6a5420906314ea33234a2b751a0ce5ef44a2cbc2
MuraliHamsa <muralikncse@gmail.com>
2018-10-23 07:10:35 +0530

    manu

--- resources/views/updateManufacturer.blade.php
+++ resources/views/updateManufacturer.blade.php
@@ -16,9 +16,10 @@
                     @foreach($manufacturers as $manufacturer)
                     <tr>
                         <td><a href="{{ URL::to('/') }}/updateManufacturerDetails?id={{ $manufacturer->id }}">{{ $manufacturer->id }}</td>
-                        <td>{{ $manufacturer->proc->name }}</td>
+                          
+                        <td>{{ $manufacturer->proc != null ?$manufacturer->proc->name:'' }}</td>
                         <td>{{ $manufacturer->address }}</td>
-                        <td>{{ $manufacturer->proc->contact}}</td>
+                        <td>{{ $manufacturer->proc != null ? $manufacturer->proc->contact:''}}</td>
                         <td><a href="{{ URL::to('/') }}/updateManufacturerDetails?id={{ $manufacturer->id }}" class="btn btn-primary btn-sm">Edit</a>
                          <a class="btn btn-sm btn-success btn-sm" name="addenquiry" href="{{ URL::to('/') }}/manuenquiry?projectId={{ $manufacturer->id }}" style="color:white;font-weight:bold;padding: 6px;">Add Enquiry</a>
                      </td>
