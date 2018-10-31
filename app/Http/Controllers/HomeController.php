0c70c76658978cbc3251cacf380044980ac87b2f
muraliyadav <34570686+MuraliHamsa@users.noreply.github.com>
2018-10-31 17:05:12 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3678,7 +3678,7 @@ date_default_timezone_set("Asia/Kolkata");
                 }
             }
 
-dd( $projectids );
+
             $rmcInt = explode(",", $rmc);
             if($rmcInt[0] != "null"){
                 if(count($projectids) != 0){
@@ -3811,7 +3811,7 @@ $upvcInt = explode(",", $upvc);
                 }
             }
              
-
+dd( $projectids );
 
         $checking = AssignStage::where('user_id',Auth::user()->id)->pluck('project_ids')->first();
         if($checking != null){
