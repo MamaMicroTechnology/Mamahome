2563953c1720fabec5567c55a24e1c7fd687aabd
MuraliHamsa <muralikncse@gmail.com>
2019-01-07 08:40:13 +0530

    sa

--- app/CustomerProjectAssign.php
+++ app/CustomerProjectAssign.php
@@ -1,16 +1,13 @@
 <?php
 
+
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
 class CustomerProjectAssign extends Model
 {
-<<<<<<< HEAD
-    //
-=======
       public function user(){
         return $this->belongsTo('App\User','user_id','id');
     }
->>>>>>> master
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
     }
-
+// $this->app->alias('bugsnag.logger', \Illuminate\Contracts\Logging\Log::class);
+// $this->app->alias('bugsnag.logger', \Psr\Log\LoggerInterface::class);
     /**
      * Register any application services.
      *
