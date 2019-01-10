86769822e238fffae1c85daae63e60e54eb6b477
MuraliHamsa <muralikncse@gmail.com>
2019-01-10 12:47:20 +0530

    sa

--- config/app.php
+++ config/app.php
@@ -178,9 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         Barryvdh\Cors\ServiceProvider::class,
-        RenatoMarinho\LaravelPageSpeed\ServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
-        Spatie\LaravelImageOptimizer\ImageOptimizerServiceProvider::class,
      
     ],
 
